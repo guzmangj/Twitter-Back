c10f3fd47c8aec1cecc4f571377bed1565999538
Pataty22 <110777470+Pataty22@users.noreply.github.com>
2023-06-06 12:00:35 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,9 @@
-/**
- * No hay una única forma de organizar las rutas de un sitio web.
- * Una alternativa podría ser organizar las rutas por entidad:
- */
-
 const userRoutes = require("./userRoutes");
-const authRoutes = require("./authRoutes");
-// const articleRoutes = require("./articleRoutes");
-// const commentRoutes = require("./commentRoutes");
-const tweetRoutes = require("./tweetRoutes");
-/**
- * Otra alternativa podría ser organizar las rutas según su nivel de
- * privacidad (ej: si son rutas públicas o privadas).
- *
- * En `publicRoutes` podrían estar las rutas relacionadas con páginas como
- * Home, Contacto y Sobre Nosotros. En `privateRoutes` podrían estar las rutas
- * relacionados al Panel de Control (Admin). Notar que si se está construyendo
- * una API esta alternativa no tendría sentido.
- */
 
-const publicRoutes = require("./publicRoutes");
-// const privateRoutes = require("./privateRoutes");
+const tweetRoutes = require("./tweetRoutes");
 
 module.exports = (app) => {
-  /**
-   * Notar que si el sitio está en español, tiene sentido que las URLs que se
-   * ven en la barra de direcciiones del navegador también lo estén. No así los
-   * nombres de variables, funciones, etc, que siempre se recomienda que estén
-   * en inglés.
-   */
-
   app.use("/usuarios", userRoutes);
-  app.use("/", authRoutes);
+  //token routes
   app.use("/", tweetRoutes);
-  app.use("/", publicRoutes);
 };
-
-// PD: Recordar que es muy importante el orden en que se definen las rutas.
