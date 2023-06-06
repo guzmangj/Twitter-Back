ee7aaa3509bc6c86ed9fd15148e242c3550501b1
guzmangj <guzmangj96@gmail.com>
2023-06-06 15:40:07 -0300

    user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,17 @@
-const Tweet = require("../models/Tweet");
 const User = require("../models/User");
-const { formattedData } = require("./tweetController");
 
 async function show(req, res) {
-  const profile = true;
-  const id = req.params.id;
-  const user = await User.findById(id);
-  const allTweets = await Tweet.find({ user: id }).populate({ path: "user" });
-  for (let i = 0; i < allTweets.length; i++) {
-    allTweets[i].formattedData = formattedData(allTweets[i].date);
-  }
-
-  return res.json({ allTweets, user, profile });
+  const user = await User.findById({ id: req.params.id });
+  return res.json({
+    firstname: user.firstname,
+    lastname: user.lastname,
+    email: user.email,
+    username: user.username,
+    image: user.image,
+    description: user.description,
+    followers: user.followers,
+    following: user.following,
+  });
 }
 
 async function store(req, res) {
