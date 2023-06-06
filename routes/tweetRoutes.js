0e23e7338e985c4798e0b5745bfdee9af10a67ba
Pataty22 <110777470+Pataty22@users.noreply.github.com>
2023-06-06 12:25:17 -0300

    .

--- routes/tweetRoutes.js
+++ routes/tweetRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const tweetController = require("../controllers/tweetController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
-router.get("/tweets", tweetController.index);
-router.post("/tweets", tweetController.store);
+//router.get("/tweets", tweetController.index);
+//router.post("/tweets", tweetController.store);
 router.delete("/tweets/:id", tweetController.destroy);
 
 module.exports = router;
