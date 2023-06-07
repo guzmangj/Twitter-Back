56ed035f1c0e996b0b80a2dc65fc1ebd3d393001
guzmangj <guzmangj96@gmail.com>
2023-06-07 17:06:04 -0300

    corrección login

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,17 +8,14 @@ async function token(req, res) {
   if (!user) {
     return res.json("Credenciales inválidas");
   } else {
-    bcrypt.compare(req.body.password, user.password, function (err, result) {
-      if (err) {
-        console.log("Error");
-      }
-      if (result) {
-        const token = jwt.sign({ id: user.id }, process.env.SECRET);
-        return res.json({ token });
-      } else {
-        return res.json("Contraseña inválida");
-      }
-    });
+    const result = await user.comparePassword(req.body.password);
+    console.log(result);
+    if (result) {
+      const token = jwt.sign({ id: user.id }, process.env.SECRET);
+      return res.json({ token });
+    } else {
+      return res.json("Contraseña inválida");
+    }
   }
 }
 
